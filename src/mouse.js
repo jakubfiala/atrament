88e2fb0d194ef6400246f81a3bf54f0a41b32671
jakubfiala <j4kub.fiala@gmail.com>
2020-01-01 15:29:55 +0000

    simplified Mouse and Point classes

--- src/mouse.js
+++ src/mouse.js
@@ -1,29 +1,13 @@
 // make a class for Point
 class Point {
   constructor(x, y) {
-    this._x = x;
-    this._y = y;
-  }
-
-  get x() {
-    return this._x;
-  }
-
-  get y() {
-    return this._y;
-  }
-
-  set x(x) {
-    this._x = x;
-  }
-
-  set y(y) {
-    this._y = y;
+    this.x = x;
+    this.y = y;
   }
 
   set(x, y) {
-    this._x = x;
-    this._y = y;
+    this.x = x;
+    this.y = y;
   }
 }
 
@@ -31,51 +15,10 @@ class Point {
 class Mouse extends Point {
   constructor() {
     super(0, 0);
-    this._down = false;
-    this._px = 0;
-    this._py = 0;
+    this.down = false;
+    this.px = 0;
+    this.py = 0;
   }
-
-  get down() {
-    return this._down;
-  }
-
-  set down(d) {
-    this._down = d;
-  }
-
-  get x() {
-    return this._x;
-  }
-
-  get y() {
-    return this._y;
-  }
-
-  set x(x) {
-    this._x = x;
-  }
-
-  set y(y) {
-    this._y = y;
-  }
-
-  get px() {
-    return this._px;
-  }
-
-  get py() {
-    return this._py;
-  }
-
-  set px(px) {
-    this._px = px;
-  }
-
-  set py(py) {
-    this._py = py;
-  }
-
 }
 
-export default Mouse;
+module.exports = Mouse;

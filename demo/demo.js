405d6bfbe8123b68af3210ce8a1f3b525a4ce2de
jakubfiala <j4kub.fiala@gmail.com>
2020-01-05 13:00:57 +0000

    added a simulate function to demo

--- demo/demo.js
+++ demo/demo.js
@@ -47,4 +47,40 @@ atrament.addEventListener('strokeend', () => log('event: strokeend'));
 atrament.recordStrokes = true;
 atrament.addEventListener('strokerecorded', ({ stroke }) => {
   log(`event: strokerecorded - ${stroke.points.length} points`);
+  console.log(JSON.stringify(stroke));
+});
+
+// this object was obtained from 'strokerecorded'
+const testStroke = { "points": [{ "x": 419, "y": 161 }, { "x": 410, "y": 177 }, { "x": 390, "y": 240 }, { "x": 386, "y": 277 }, { "x": 386, "y": 318 }, { "x": 386, "y": 350 }, { "x": 386, "y": 365 }, { "x": 388, "y": 378 }, { "x": 392, "y": 384 }, { "x": 426, "y": 404 }, { "x": 456, "y": 411 }, { "x": 489, "y": 411 }, { "x": 539, "y": 411 }, { "x": 566, "y": 406 }, { "x": 586, "y": 390 }, { "x": 603, "y": 372 }, { "x": 614, "y": 355 }, { "x": 623, "y": 329 }, { "x": 628, "y": 310 }, { "x": 630, "y": 293 }, { "x": 630, "y": 276 }, { "x": 630, "y": 257 }, { "x": 630, "y": 250 }, { "x": 630, "y": 244 }, { "x": 630, "y": 240 }, { "x": 629, "y": 239 }, { "x": 628, "y": 238 }, { "x": 627, "y": 238 }, { "x": 627, "y": 238 }, { "x": 627, "y": 238 }], "weight": 2, "smoothing": 0.85, "color": "#000000", "adaptiveStroke": true };
+
+const simButton = document.getElementById('simulate');
+
+// utility to add delay to drawing steps
+const sleep = async time => new Promise((r) => setTimeout(r, time));
+
+simButton.addEventListener('click', async e => {
+  e.preventDefault();
+  // don't want to modify original data
+  const points = testStroke.points.slice();
+
+  const firstPoint = points.shift();
+  atrament.beginStroke(firstPoint.x, firstPoint.y);
+
+  let prevPoint = firstPoint;
+  while (points.length > 0) {
+    const point = points.shift();
+
+    // the `draw` method accepts the current real coordinates
+    // (i. e. actual cursor position), and the previous processed (filtered)
+    // position. It returns an object with the current processed position.
+    const { x, y } = atrament.draw(point.x, point.y, prevPoint.x, prevPoint.y);
+
+    // the processed position is the one where the line is actually drawn to
+    // so we have to store it and pass it to `draw` in the next step
+    prevPoint = { x, y };
+
+    await sleep(50);
+  }
+
+  atrament.endStroke(prevPoint.x, prevPoint.y);
 });

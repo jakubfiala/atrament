e80693086eb4a5841cacc24da74c0a8cffa37c96
jakubfiala <j4kub.fiala@gmail.com>
2025-01-19 10:48:43 +0100

    check value of PointerEvent.button for all events

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -84,7 +84,11 @@ const lineDistance = (x1, y1, x2, y2) => {
 /* eslint-enable no-param-reassign */
 
 const pointerEventHandler = (handler) => (event) => {
-  if (!event.isPrimary) {
+  // Ignore pointers such as additional touches on a multi-touch screen,
+  // as well as all mouse buttons other than the left button.
+  // `PointerEvent.button` is -1 if no button is pressed, but also for `pointermove` events,
+  // and this value is relevant to us. See https://w3c.github.io/pointerevents/#the-button-property
+  if (!event.isPrimary || event.button > 0) {
     return;
   }
 
